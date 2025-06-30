4edee194f05eb92726b1809a8f942671366887c5
pDiosquez467 <pabloDiosquez467@gmail.com>
2025-06-30 16:55:23 -0300

    Agrego un archivo para manejar conexión a la BD

---
+++ db/conexion.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+});
+
+pool.connect()
+  .then(() => console.log('✅ Conexión a DB exitosa'))
+  .catch(err => console.error('❌ Error al conectar a DB:', err));
+
+module.exports = pool;
