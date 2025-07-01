9c32f4b282ee63b2eed917c513ab4ef03fc25e64
pDiosquez467 <pabloDiosquez467@gmail.com>
2025-07-01 09:26:00 -0300

    Agrego método PUT para pokemons

--- backend/controllers/pokemons.js
+++ backend/controllers/pokemons.js
@@ -1,16 +1,16 @@
 const db = require('../db/conexion.js')
 
 async function getAll() {
-    const result = await db.query('SELECT * FROM pokemons')
-    return result.rows
+  const result = await db.query('SELECT * FROM pokemons')
+  return result.rows
 }
 
 async function get(id) {
-    const result = await db.query(
-        'SELECT * FROM pokemons WHERE id = $1;',
-        [id]
-    )
-    return result.rows[0]
+  const result = await db.query(
+    'SELECT * FROM pokemons WHERE id = $1;',
+    [id]
+  )
+  return result.rows[0]
 }
 
 async function create(data) {
@@ -39,18 +39,59 @@ async function create(data) {
   return result.rows[0];
 }
 
+async function update(id, data) {
+  const fields = [];
+  const values = [];
+  let i = 1;
+
+  if (data.nombre !== undefined) {
+    fields.push(`nombre = $${i}`);
+    values.push(data.nombre);
+    i++;
+  }
+
+  if (data.tipo !== undefined) {
+    fields.push(`tipo = $${i}`);
+    values.push(data.tipo);
+    i++;
+  }
+
+  if (data.habilidades !== undefined) {
+    fields.push(`habilidades = $${i}`);
+    values.push(data.habilidades);
+    i++;
+  }
+
+  if (fields.length === 0) {
+    return undefined
+  }
+
+  values.push(id)
+  const query = `
+    UPDATE pokemons 
+    SET ${fields.join(', ')} 
+    WHERE id = $${i}
+    RETURNING *;
+  `
+
+  const result = await db.query(query, values)
+  return result.rows[0]
+}
+
+
 async function deleteByID(id) {
-    const result = await db.query(
-        `DELETE FROM pokemons WHERE id = $1 RETURNING *`,
-        [id]
-    );
-    return result.rows[0];
+  const result = await db.query(
+    `DELETE FROM pokemons WHERE id = $1 RETURNING *`,
+    [id]
+  );
+  return result.rows[0];
 
 }
 
 module.exports = {
-    getAll,
-    get,
-    create,
-    deleteByID
+  getAll,
+  get,
+  create,
+  update,
+  deleteByID
 };
--- backend/routes/pokemons.js
+++ backend/routes/pokemons.js
@@ -31,38 +31,55 @@ router.get('/:id', async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
-  try {
-    const nuevo = await controller.create(req.body)
-    res.status(201).json(nuevo)
-  } catch (error) {
-    res.status(500).json({ error: error.message })
-  }
+    try {
+        const nuevo = await controller.create(req.body)
+        res.status(201).json(nuevo)
+    } catch (error) {
+        res.status(500).json({ error: error.message })
+    }
 })
 
-router.put('/:id', (req, res) => {
-    res.json({ msg: "Status OK" })
+router.put('/:id', async (req, res) => {
+  try {
+    const id = Number(req.params.id);
+
+    if (isNaN(id)) {
+      return res.status(400).json({ error: 'ID inválido' });
+    }
+
+    const updated = await controller.update(id, req.body);
+
+    if (!updated) {
+      return res.status(404).json({ error: 'Pokémon no encontrado o sin cambios' });
+    }
+
+    res.status(200).json(updated);
+
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 })
 
 router.delete('/:id', async (req, res) => {
-  try {
-    const id = Number(req.params.id);
-    if (isNaN(id)) {
-      return res.status(400).json({ error: 'ID inválido' })
+    try {
+        const id = Number(req.params.id);
+        if (isNaN(id)) {
+            return res.status(400).json({ error: 'ID inválido' })
+        }
+
+        const deleted = await controller.deleteByID(id);
+
+        if (!deleted) {
+            return res.status(404).json({ error: 'Pokémon no encontrado' })
+        }
+
+        res.json({
+            msg: 'Pokémon eliminado correctamente',
+            data: deleted
+        })
+    } catch (error) {
+        res.status(500).json({ error: error.message })
     }
-
-    const deleted = await controller.deleteByID(id);
-
-    if (!deleted) {
-      return res.status(404).json({ error: 'Pokémon no encontrado' })
-    }
-
-    res.json({
-      msg: 'Pokémon eliminado correctamente',
-      data: deleted
-    })
-  } catch (err) {
-    res.status(500).json({ error: err.message })
-  }
 });
 
 
