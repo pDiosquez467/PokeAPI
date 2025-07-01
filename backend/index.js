1ec79450102135188025b67bfa3a3a251b1683e1
pDiosquez467 <pabloDiosquez467@gmail.com>
2025-07-01 09:36:48 -0300

    Agrego router para entrenadores

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,9 @@ const PORT = process.env.PORT || 5000
 const pokemonsRoutes = require('./routes/pokemons')
 app.use('/api/pokemons', pokemonsRoutes)
 
+const entrandoresRoutes = require('./routes/entrenadores')
+app.use('/api/entrenadores', entrandoresRoutes)
+
 app.use(express.json())
 
 app.get('/api/health', (req, res) => {
