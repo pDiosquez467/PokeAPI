cac6292e19a18dd7c50f8acdc276626fe1671437
pDiosquez467 <pabloDiosquez467@gmail.com>
2025-07-07 07:58:14 -0300

    Agrego método GET by ID de combates

--- backend/controllers/combates.js
+++ backend/controllers/combates.js
@@ -1,12 +1,17 @@
+const { text } = require('express')
 const db = require('../db/conexion')
-const { get } = require('../routes/pokemons')
 
 const getAll = async () => {
     const combates = await db.query(`SELECT * FROM combates;`)
     return combates.rows
 }
 
+const get = async (combateId) => {
+    const combate = await db.query(`SELECT * FROM combates WHERE id = $1;`, [combateId])
+    return combate.rows[0]
+}
 
 module.exports = {
-    getAll
+    getAll,
+    get, 
 }
--- backend/routes/combates.js
+++ backend/routes/combates.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const controller = require('../controllers/combates')
 
-router.get('/combates', async (req, res) => {
+router.get('/', async (req, res) => {
     try {
         const combates = await controller.getAll()
         res.status(200).send({ status: 'OK', data: combates }) 
@@ -10,3 +10,26 @@ router.get('/combates', async (req, res) => {
         res.status(500).send({ status: 'Error', error: error })
     }
 })
+
+router.get('/:id', async (req, res) => {
+    try {
+
+        const id = Number(req.params.id)
+
+        if (isNaN(id)) {
+            return res.status(400).send({ status: 'OK', error: 'ID inválido' })
+        }
+
+        const combate = await controller.get()
+
+        if (!combate) {
+            return res.status(404).send({ status: 'Combate NO encontrado', data: undefined })
+        }
+
+        res.status(200).send({ status: 'OK', data: combate })
+    } catch (error) {
+        res.status(500).send({ status: 'Error', error: error })
+    }
+})
+
+module.exports = router
