90895b20d77d20874d205ca8e0ce3eee4fc26b53
pDiosquez467 <pabloDiosquez467@gmail.com>
2025-07-08 10:35:40 -0300

    Agrego refac para incluir al service

--- src/controllers/pokemonsController.js
+++ src/controllers/pokemonsController.js
@@ -1,20 +1,27 @@
+const pokemonService = require('../services/pokemonsService')
+
 const getAllPokemons = async (req, res) => {
+    const allPokemons = await pokemonService.getAllPokemons()
     res.send('Get All pokemons')
 }
 
 const getOnePokemon = async (req, res) => {
+    const pokemon = await pokemonService.getOnePokemon()
     res.send('Get an existing pokemon')
 }
 
 const createNewPokemon = async (req, res) => {
+    const createdPokemon = await pokemonService.createNewPokemon()
     res.send('Create a new pokemon')
 }
 
 const updateOnePokemon = async (req, res) => {
+    const updatedPokemon = await pokemonService.updateOnePokemon()
     res.send('Update an existing pokemon')
 }
 
 const deleteOnePokemon = async (req, res) => {
+    const deletedPokemon = await pokemonService.deleteOnePokemon()
     res.send('Delete an existing pokemon')
 }
 
