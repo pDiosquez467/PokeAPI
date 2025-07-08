97af97cc25806013e4bf89d453b4dc989b7f9310
pDiosquez467 <pabloDiosquez467@gmail.com>
2025-07-08 10:31:32 -0300

    Agrego esqueleto de service de pokemons

---
+++ src/services/pokemonsService.js
@@ -0,0 +1,27 @@
+const getAllPokemons = async () => {
+    return 
+}
+
+const getOnePokemon = async () => {
+    return 
+}
+
+const createNewPokemon = async () => {
+    return 
+}
+
+const updateOnePokemon = async () => {
+    return 
+}
+
+const deleteOnePokemon = async () => {
+    return 
+}
+
+module.exports = {
+    getAllPokemons,
+    getOnePokemon,
+    createNewPokemon,
+    updateOnePokemon,
+    deleteOnePokemon
+}
