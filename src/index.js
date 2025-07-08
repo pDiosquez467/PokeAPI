94d7edeb545a7b9d8090d1f33fd8eb14359f7a8b
pDiosquez467 <pabloDiosquez467@gmail.com>
2025-07-08 09:31:43 -0300

    Agrego router para pokemons

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT ?? 3000
 
+const v1PokemonRouter = require('./v1/routes/pokemons')
+
 app.use(express.json())
+
+app.use('/api/v1/pokemons', v1PokemonRouter)
 
 app.get('/', (req, res) => {
     res.send(`<h1>It's working!!</h1>`)
