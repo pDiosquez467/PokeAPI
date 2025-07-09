1b17094f88957490a7b0e98b12b0ae7f5a175a31
pDiosquez467 <pabloDiosquez467@gmail.com>
2025-07-09 15:17:30 -0300

    Agrego tests GET All y POST

--- tests/index.test.js
+++ tests/index.test.js
@@ -12,3 +12,40 @@ describe('GET /', () => {
     expect(response.status).toBe(200)
   })
 })
+
+describe('GET /api/v1/pokemons', () => {
+  test('Should respond with a 200 status code and an array', async () => {
+    const response = await request(app).get('/api/v1/pokemons')
+    expect(response.status).toBe(200)
+    expect(Array.isArray(response.body.data)).toBe(true)
+  })
+})
+
+describe('POST /api/v1/pokemons', () => {
+  const newPokemon = {
+    nombre: 'Pikachu',
+    tipo: 'Eléctrico',
+    nivel_poder: 42,
+    altura: 0.4,
+    peso: 6.0
+  }
+
+  test('Should respond with 201 and return the created Pokémon with an ID', async () => {
+    const response = await request(app)
+      .post('/api/v1/pokemons')
+      .send(newPokemon)
+
+    expect(response.status).toBe(201)
+    expect(response.body.data).toHaveProperty('id')
+    expect(response.body.data.nombre).toBe(newPokemon.nombre)
+  })
+
+
+  test('Should have content-type: application/json', async () => {
+    const response = await request(app)
+      .post('/api/v1/pokemons')
+      .send(newPokemon)
+
+    expect(response.header['content-type']).toMatch(/application\/json/)
+  })
+})
