75ff293f70d9550628f5c7bcbf4280a4774ce7da
pDiosquez467 <pabloDiosquez467@gmail.com>
2025-07-14 13:06:27 -0300

    Agrego configuración y reestr requerido por Prisma

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
 
-const v1PokemonRouter = require('./v1/routes/pokemonsRoutes')
+const v1PokemonRouter = require('./v2/routes/pokemonsRoutes')
 
 app.use(express.json())
 
-app.use('/api/v1/pokemons', v1PokemonRouter)
+app.use('/api/v2/pokemons', v1PokemonRouter)
 
 app.get('/', (req, res) => {
     res.send(`<h1>It's working!!</h1>`)
