09b17109a2870d0503099bac83dfd197eb112c3f
pDiosquez467 <pabloDiosquez467@gmail.com>
2025-07-15 20:08:07 -0300

    Agrego manejo de excep en métodos getOnePoke, getAllPoke

--- src/models/Pokemons.js
+++ src/models/Pokemons.js
@@ -1,9 +1,31 @@
 const prisma = require('../prisma/prisma')
 
 async function getAllPokemons() {
-    return await prisma.pokemons.findMany()
-} 
+    try {
+        return await prisma.pokemons.findMany()
+    } catch (error) {
+        throw { status: error?.status || 500, message: error?.message || error }
+    }
+}
+
+async function getOnePokemon(pokemonId) {
+    try {
+        const pokemon = await prisma.pokemons.findFirst({
+            where: {
+                id: pokemonId
+            }
+        })
+
+        if (!pokemon) {
+            throw { status: 'FAILED', message: `Can't find pokemon with ID '${pokemonId}'` }
+        }
+
+    } catch (error) {
+        throw { status: error?.status || 500, message: error?.message || error }
+    }
+}
 
 module.exports = {
-    getAllPokemons
+    getAllPokemons,
+    getOnePokemon
 }
