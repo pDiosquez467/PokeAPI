87a2d21eed407d01c2f24a67227714b53e2caa33
pDiosquez467 <pabloDiosquez467@gmail.com>
2025-07-15 20:10:44 -0300

    Agrego validación pokemonId en getOnePokemon

--- src/controllers/pokemonsController.js
+++ src/controllers/pokemonsController.js
@@ -3,12 +3,32 @@ const pokemonsModel = require('../models/Pokemons')
 async function getAllPokemons(req, res) {
     try {
         const allPokemons = await pokemonsModel.getAllPokemons()
-        res.status(200).send({ status:'OK', data: allPokemons })
+
+        res.status(200).send({ status: 'OK', data: allPokemons })
+
+    } catch (error) {
+        res.status(500).send({ status: 'FAILED', error: error?.message || error })
+    }
+}
+
+async function getOnePokemon(req, res) {
+    try {
+        const pokemonId = Number(req.params.pokemonId)
+
+        if (isNaN(pokemonId)) {
+            res.status(400).send({ status: 'FAILED', error: 'Invalid ID' })
+        }
+
+        const pokemon = await pokemonsModel.getOnePokemon(pokemonId)
+
+        res.status(200).send({ status: 'OK', data: pokemon })
+
     } catch (error) {
         res.status(500).send({ status: 'FAILED', error: error?.message || error })
     }
 }
 
 module.exports = {
-    getAllPokemons
+    getAllPokemons,
+    getOnePokemon
 }
