cd37e29047001c8fecb389b8cd731c51652fc04c
pDiosquez467 <pabloDiosquez467@gmail.com>
2025-07-19 16:05:50 -0300

    Agrego middleware - validateId

---
+++ middlewares/validateId.js
@@ -0,0 +1,14 @@
+async function validateId(req, res, next) {
+    let { id } = req.params 
+    id = Number(id)
+
+    if (isNaN(id)) {
+        return res.status(400).send( {
+            status: 'FAILED',
+            data: { error: 'Invalid ID' }
+        })
+    }
+
+    req.params.id = id
+    next()
+}
