f0c34c1e19a38c54efcdb5d1c73e77bba970f67f
pDiosquez467 <pabloDiosquez467@gmail.com>
2025-07-19 20:49:41 -0300

    Agrego cambios en las rutas

--- src/v2/routes/pokemonsRoutes.js
+++ src/v2/routes/pokemonsRoutes.js
@@ -2,15 +2,16 @@ const express = require('express')
 const router = express.Router()
 
 const pokemonController = require('../../controllers/pokemonsController')
-const validatePokemonId = require('../../middlewares/validateId')
+const { validateId: validatePokemonId } = require('../../middlewares/validateId')
 
 router.route('/')
     .get(pokemonController.getAllPokemons)
     .post(pokemonController.createOnePokemon)
 
-router.route('/:pokemonId', validatePokemonId)
-    .get(pokemonController.getOnePokemon)
-    .delete(pokemonController.deleteOnePokemon)
-    .patch(pokemonController.updateOnePokemon)
+router.get('/id/:id', validatePokemonId, pokemonController.getOnePokemon)
+
+router.delete('/id/:id', validatePokemonId, pokemonController.deleteOnePokemon)
+
+router.patch('/id/:id', validatePokemonId, pokemonController.updateOnePokemon)
 
 module.exports = router
