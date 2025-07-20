461fead8d89f32e201892a5903696196a9ed264c
pDiosquez467 <pabloDiosquez467@gmail.com>
2025-07-20 16:06:09 -0300

    Agrego unit test para GET By ID de pokemons

---
+++ test/pokemons.test.js
@@ -0,0 +1,21 @@
+const request = require('supertest')
+const app = require('../src/app')
+
+describe('', () => {
+    test('Should return an existing Pokemon (status 200)', async () => {
+        const res = await request(app).get('/api/v2/pokemons/id/2')
+        expect(res.statusCode).toBe(200)
+        expect(res.body.data).toHaveProperty('id', 2)
+        expect(res.body.data).toHaveProperty('nombre') 
+    })
+
+    test('Should return "Pokemon NOT FOUND"', async () => {
+        const res = await request(app).get('/api/v2/pokemons/id/9999')
+        expect(res.statusCode).toBe(404)
+    })
+
+    test('Should return status 400 BAD REQUEST', async () => {
+        const res = await request(app).get('/api/v2/pokemons/id/abc')
+        expect(res.statusCode).toBe(400)
+    })
+})
