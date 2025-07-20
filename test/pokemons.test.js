03d73774c2c87c2c26ee1fb2db6c8c678f2988f2
pDiosquez467 <pabloDiosquez467@gmail.com>
2025-07-20 16:31:12 -0300

    Agrego tests unitarios para GET All y GET By Id de pokemons

--- test/pokemons.test.js
+++ test/pokemons.test.js
@@ -1,12 +1,22 @@
 const request = require('supertest')
 const app = require('../src/app')
 
-describe('', () => {
+describe('GET /api/v2/pokemons', () => {
+    test('Should return an array of pokemons', async () => {
+        const res = await request(app).get('/api/v2/pokemons')
+        expect(res.statusCode).toBe(200)
+        expect(res.body).toHaveProperty('status', 'OK')
+        expect(res.body).toHaveProperty('data')
+        expect(Array.isArray(res.body.data)).toBe(true)
+    })
+})
+
+describe('GET /api/v2/pokemons/id/:id', () => {
     test('Should return an existing Pokemon (status 200)', async () => {
         const res = await request(app).get('/api/v2/pokemons/id/2')
         expect(res.statusCode).toBe(200)
         expect(res.body.data).toHaveProperty('id', 2)
-        expect(res.body.data).toHaveProperty('nombre') 
+        expect(res.body.data).toHaveProperty('nombre')
     })
 
     test('Should return "Pokemon NOT FOUND"', async () => {
