abed164921ed9cb0726711ba4db084a8e9f35b09
pDiosquez467 <pabloDiosquez467@gmail.com>
2025-07-20 10:25:49 -0300

    Agrego actualización js-docs sobre GET All Pokemons

--- src/v2/routes/pokemonsRoutes.js
+++ src/v2/routes/pokemonsRoutes.js
@@ -4,9 +4,49 @@ const router = express.Router()
 const pokemonController = require('../../controllers/pokemonsController')
 const { validateId: validatePokemonId } = require('../../middlewares/validateId')
 
-router.route('/')
-    .get(pokemonController.getAllPokemons)
-    .post(pokemonController.createOnePokemon)
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Pokemon:
+ *       type: object
+ *       required:
+ *         - name
+ *       properties:
+ *         id:
+ *           type: integer
+ *           description: The auto-generated id of the Pokemon
+ *         name:
+ *           type: string
+ *           description: The name of the Pokemon
+ */
+
+/**
+ * @swagger
+ * tags:
+ *   name: Pokemons
+ *   description: The Pokemons managing API
+ */
+
+/**
+ * @swagger
+ * /api/v2/pokemons:
+ *   get:
+ *     summary: Returns the list of all the Pokemons
+ *     tags: [Pokemon]
+ *     responses:
+ *       200:
+ *         description: The list of the Pokemons
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Pokemon'
+ */
+router.get('/', pokemonController.getAllPokemons)
+    
+router.post('/', pokemonController.createOnePokemon)
 
 router.get('/id/:id', validatePokemonId, pokemonController.getOnePokemon)
 
