4b97ced6b122cc48fae985681118d3a7771c271a
pDiosquez467 <pabloDiosquez467@gmail.com>
2025-07-21 12:53:42 -0300

    Agrego tests unitarios método deleteOnePokemon

--- test/pokemons.test.js
+++ test/pokemons.test.js
@@ -13,7 +13,7 @@ describe('GET /api/v2/pokemons', () => {
 
 describe('GET /api/v2/pokemons/id/:id', () => {
     test('Should return an existing Pokemon (status 200)', async () => {
-        const res = await request(app).get('/api/v2/pokemons/id/2')
+        const res = await request(app).get('/api/v2/pokemons/id/3')
         expect(res.statusCode).toBe(200)
         expect(res.body.data).toHaveProperty('id', 2)
         expect(res.body.data).toHaveProperty('nombre')
@@ -28,4 +28,23 @@ describe('GET /api/v2/pokemons/id/:id', () => {
         const res = await request(app).get('/api/v2/pokemons/id/abc')
         expect(res.statusCode).toBe(400)
     })
+})
+
+describe('DELETE /api/v2/pokemons/id/:id', () => {
+    test('Should return a deleted pokemon (status 200)', async () => {
+        const res = await request(app).delete('/api/v2/pokemons/id/3')
+        expect(res.statusCode).toBe(200)
+        expect(res.body.data).toHaveProperty('id', 3)
+        expect(res.body.data).toHaveProperty('nombre', 'TestMon UpUp')
+    })
+
+    test('Should return status 404 NOT FOUND', async () => {
+        const res = await request(app).delete('/api/v2/pokemons/id/0').send()
+        expect(res.statusCode).toBe(404)
+    })
+
+    // test('Should return status 400 BAD REQUEST', async () => {
+    //     const res = await request(app).delete('/api/v2/pokemons/abc').send()
+    //     expect(res.statusCode).toBe(400)
+    // })
 })
