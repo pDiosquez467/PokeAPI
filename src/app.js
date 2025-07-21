69c54a9a5d64339860d5efbcb4078a53b7f42850
pDiosquez467 <pabloDiosquez467@gmail.com>
2025-07-21 11:15:37 -0300

    Agrego router para entrenadores

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const app = express()
 
 const v2PokemonRouter = require('./v2/routes/pokemonsRoutes')
 
+const v2EntrenadorRouter = require('./v2/routes/entrenadoresRoutes')
+
 const { swaggerUi, swaggerSpec } = require('./config/swagger');
 
 app.use(express.json())
@@ -10,6 +12,8 @@ app.use(express.json())
 app.use('/api/v2/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use('/api/v2/pokemons', v2PokemonRouter)
+
+app.use('/api/v2/entrenadores', v2EntrenadorRouter)
 
 app.get('/', (req, res) => {
     res.send(`<h1>It's working!!</h1>`)
