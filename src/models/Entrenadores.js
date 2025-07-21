6aeb6263667369b4f205c3cb1667039b01000eda
pDiosquez467 <pabloDiosquez467@gmail.com>
2025-07-21 10:19:00 -0300

    Agrego método GET All entrenadores

--- src/models/Entrenadores.js
+++ src/models/Entrenadores.js
@@ -1,5 +1,14 @@
 const prisma = require('../prisma/prisma')
 
 const getAllEntrenadores = async () => {
-    return await prisma.entrenadores.findMany()
+    try {
+        return await prisma.entrenadores.findMany()
+    } catch (error) {
+        throw { status: error?.status || 500, message: error?.message || String(error) }
+    }
+}
+
+
+module.exports = {
+    getAllEntrenadores
 }
