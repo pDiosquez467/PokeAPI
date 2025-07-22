43089940efe9cf3c987c885da7b9848254b6fbcb
pDiosquez467 <pabloDiosquez467@gmail.com>
2025-07-22 08:47:48 -0300

    Agrego esqueleto del CRUD universos

---
+++ src/models/Combates.js
@@ -0,0 +1,29 @@
+const prisma = require('../prisma/prisma')
+
+const getAllCombates = async () => {
+
+}
+
+const getOneCombate = async (combateId) => {
+
+}
+
+const createOneCombate = async (newCombate) => {
+
+}
+
+const updateOneCombate = async (combateId, changes) => {
+
+}
+
+const deleteOneCombate = async (combateId) => {
+
+}
+
+module.exports = {
+    getAllCombates,
+    getOneCombate,
+    createOneCombate,
+    updateOneCombate,
+    deleteOneCombate
+}
