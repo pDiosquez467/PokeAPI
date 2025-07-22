afb8c0e2b439d2757a942240a8fdad32195b441b
pDiosquez467 <pabloDiosquez467@gmail.com>
2025-07-22 09:31:28 -0300

    Agrego uso del router de combates

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ const v2PokemonRouter = require('./v2/routes/pokemonsRoutes')
 
 const v2EntrenadorRouter = require('./v2/routes/entrenadoresRoutes')
 
+const v2CombateRouter = require('./v2/routes/combatesRoutes')
+
 const { swaggerUi, swaggerSpec } = require('./config/swagger');
 
 app.use(express.json())
@@ -14,6 +16,8 @@ app.use('/api/v2/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use('/api/v2/pokemons', v2PokemonRouter)
 
 app.use('/api/v2/entrenadores', v2EntrenadorRouter)
+
+app.use('/api/v2/combates', v2CombateRouter)
 
 app.get('/', (req, res) => {
     res.send(`<h1>It's working!!</h1>`)
