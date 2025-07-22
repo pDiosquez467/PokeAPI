146f416e336250245d013a28636cbba6ebbeda58
pDiosquez467 <pabloDiosquez467@gmail.com>
2025-07-22 09:11:22 -0300

    Agrego esqueleto impl métodos

---
+++ src/controllers/combatesController.js
@@ -0,0 +1,29 @@
+const combatesModel = require('../models/Combates')
+
+const getAllCombates = async (req, res) => {
+
+}
+
+const getOneCombate = async (req, res) => {
+    
+}
+
+const createOneCombate = async (req, res) => {
+    
+}
+
+const updateOneCombate = async (req, res) => {
+    
+}
+
+const deleteOneCombate = async (req, res) => {
+    
+}
+
+module.exports = {
+    getAllCombates,
+    getOneCombate,
+    createOneCombate,
+    updateOneCombate,
+    deleteOneCombate
+}
