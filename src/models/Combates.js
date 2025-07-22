fdde75e2704f1e36eeeb3fece83b05d205bb9da2
pDiosquez467 <pabloDiosquez467@gmail.com>
2025-07-22 09:02:58 -0300

    Agrego impl de CRUD para combates

--- src/models/Combates.js
+++ src/models/Combates.js
@@ -1,23 +1,93 @@
 const prisma = require('../prisma/prisma')
 
 const getAllCombates = async () => {
+    try {
+        return await prisma.combates.findMany()
 
+    } catch (error) {
+        throw { status: error?.status ?? 500, message: error?.message ?? error }
+    }
 }
 
 const getOneCombate = async (combateId) => {
+    try {
+        const combate = await prisma.combates.findUnique({
+            where: {
+                id: combateId
+            }
+        })
 
+        if (!combate) {
+            throw { status: 404, message: `Can't find combate with ID ${combateId}` }
+        }
+
+        return combate
+    } catch (error) {
+        throw { status: error?.status ?? 500, message: error?.message ?? error }
+    }
 }
 
 const createOneCombate = async (newCombate) => {
+    try {
+        const created = await prisma.combates.create({
+            data: {
+                ...newCombate
+            }
+        })
 
+        return created
+    } catch (error) {
+        throw { status: error?.status ?? 500, message: error?.message ?? error }
+    }
 }
 
 const updateOneCombate = async (combateId, changes) => {
+    try {
+        const exists = await prisma.combates.findUnique({
+            where: {
+                id: combateId
+            }
+        })
 
+        if (!exists) {
+            throw { status: 404, message: error?.message ?? error }
+        }
+
+        const updated = await prisma.combates.update({
+            where: {
+                id: combateId
+            },
+            data: changes
+        })
+
+        return updated
+    } catch (error) {
+        throw { status: error?.status ?? 500, message: error?.message ?? error }
+    }
 }
 
 const deleteOneCombate = async (combateId) => {
+    try {
+        const exists = await prisma.combates.findUnique({
+            where: {
+                id: combateId
+            }
+        })
 
+        if (!exists) {
+            throw { status: 404, message: `Can't find combate with ID ${combateId}` }
+        }
+
+        const deleted = await prisma.combates.delete({
+            where: {
+                id: combateId
+            }
+        })
+
+        return deleted
+    } catch (error) {
+        throw { status: error?.status ?? 500, message: error?.message ?? error }
+    }
 }
 
 module.exports = {
